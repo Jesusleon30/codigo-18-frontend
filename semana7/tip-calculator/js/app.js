1a88490f21897fb309c15fc5f26aca779dbfdf7b
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-03-14 05:07:21 +0100

    update semana 7 on-click in html

---
+++ semana7/tip-calculator/js/app.js
@@ -0,0 +1,87 @@
+const containerBill = document.querySelector("#contailer-bill");
+const containerPercentages = document.querySelector("#container-percentage");
+const inputBill = document.querySelector("#input-bill");
+
+// array con los valores de los botons
+const percentages = [
+  {
+    type: "button",
+    value: "5%",
+  },
+  {
+    type: "button",
+    value: "10%",
+  },
+  {
+    type: "button",
+    value: "20%",
+  },
+  {
+    type: "button",
+    value: "25%",
+  },
+  {
+    type: "button",
+    value: "50%",
+  },
+  {
+    type: "input",
+    value: "0",
+  },
+];
+
+// click
+containerBill.onclick = function () {
+  inputBill.focus();
+};
+
+inputBill.onkeyup = function (event) {
+  const inputValue = event.target.value;
+
+  if (inputValue[0] === "0") {
+    containerBill.classList.add("border", "border-red-500");
+  } else {
+    containerBill.classList.remove("border", "border-red-500");
+  }
+
+  if (isNaN(inputValue)) {
+    this.value = inputValue.slice(0, -1);
+  }
+};
+
+function renderButton(text) {
+  return `<button>${text}</button>`;
+}
+
+function renderInput() {
+  return `<input
+            type="text"
+            class="outline-none p-2 bg-[#F3F8FB] rounded-md"
+            placeholder="Custom"
+          />`;
+}
+
+// paso 1 es limpiar el contenido del container
+containerPercentages.innerHTML = "";
+
+percentages.forEach(function (percentage) {
+  const html =
+    percentage.type === "button"
+      ? renderButton(percentage.value)
+      : renderInput();
+
+  containerPercentages.innerHTML += html;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
