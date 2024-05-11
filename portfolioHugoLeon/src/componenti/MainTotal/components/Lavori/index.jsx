55adc4ca459a4b647f10722f1f5d728918eb80c9
Jesus Hugo Retamozo Leon <jesusretamozo@hotmail.it>
2024-05-12 01:10:20 +0200

    portfolio Hugo leon

--- portfolioHugoLeon/src/componenti/MainTotal/components/Lavori/index.jsx
+++ portfolioHugoLeon/src/componenti/MainTotal/components/Lavori/index.jsx
@@ -3,7 +3,49 @@ import "./css/index.css";
 export default function Lavori() {
   return (
     <>
-      <div>
+      <div >
+        <div className="flex gap-4 justify-center items-center">
+        <section className="profilo">
+          <div className="div-profilo text flex flex-col">
+            <p className=" text-xl">siti rapidi</p>
+
+            <ul>
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/portfolio-html-css-js/challenges/textoAnimation/index.html" target="_blank">
+                  text-animation
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://jesusleon30.github.io/codigo-18-frontend/portfolio-html-css-js/challenges/testoVideobg/index.html"
+                  target="_blank"
+                >
+                  video-blog
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/portfolio-html-css-js/challenges/menu/scatolaMenu/index.html" target="_blank">
+                  menu 3D
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/portfolio-html-css-js/challenges/menu/menu-mobile-hamburger-css-js/index.html" target="_blank">
+                  hamburger js
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/portfolio-html-css-js/challenges/galleria1/index.html" target="_blank">
+                  robots
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+
         <section className="profilo">
           <div className="div-profilo text flex flex-col">
             <p className=" text-xl">siti rapidi</p>
@@ -44,6 +86,122 @@ export default function Lavori() {
             </ul>
           </div>
         </section>
+
+      </div>
+
+        <div className="flex gap-4 justify-center items-center" >
+        <section className="profilo">
+          <div className="div-profilo text flex flex-col">
+            <p className=" text-xl">HTML - CSS</p>
+
+            <ul>
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana1/challenges/profilo1/index.html" target="_blank">
+                  Profilo 1
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://jesusleon30.github.io/codigo-18-frontend/semana1/challenges/profilo2/index.html"
+                  target="_blank"
+                >
+                  Profilo 2
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana2/challenges/tip-calculator/index.html" target="_blank">
+                  Tip-calculator
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana3/challenges/multi-step-form/index.html" target="_blank">
+                  Multi-step-form
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana3/challenges/sunnyside/index.html" target="_blank">
+                  SunnySide
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="profilo">
+          <div className="div-profilo text flex flex-col">
+            <p className=" text-xl">HTML - CSS - TAILWIND - BOOSTRAP</p>
+
+            <ul>
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana4/challenges/e-commerce-tailwind/index.html" target="_blank">
+                  e-commerce
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://jesusleon30.github.io/codigo-18-frontend/semana4/challenges/spotify/index.html"
+                  target="_blank"
+                >
+                 clon - Spotify
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana4/challenges/testimonials/index.html" target="_blank">
+                  Testimonials
+                </a>
+              </li>
+
+              <li>
+                <a href="semana5/challenges/bootstrap/index.html" target="_blank">
+                  boostrap
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana5/challenges/instagram/index.html" target="_blank">
+                  clon - instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+        </section>
+        <section className="profilo">
+          <div className="div-profilo text flex flex-col">
+            <p className=" text-xl">HTML - CSS - TAILWIND - BOOSTRAP - JAVASCRIPT</p>
+
+            <ul>
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana7/login/" target="_blank">
+                  Login
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="https://jesusleon30.github.io/codigo-18-frontend/semana7/tip-calculator/index.html"
+                  target="_blank"
+                >
+                 tip-calculator js
+                </a>
+              </li>
+
+              <li>
+                <a href="https://jesusleon30.github.io/codigo-18-frontend/semana8/index.html" target="_blank">
+                  Todolist
+                </a>
+              </li>
+
+            </ul>
+          </div>
+        </section>
+
+        </div>
+
       </div>
     </>
   );
